15dcef46f9409213f00ab52b825e4a285d935881
Rizky Khapidsyah <rizkykhapidsyah@gmail.com>
2021-07-17 14:40:50 +0700

    Add project files.

---
+++ SK-Lib-math.h-floor-Bagian1__C/Source.c
@@ -0,0 +1,12 @@
+#include <stdio.h>      /* printf */
+#include <math.h>       /* floor */
+#include <conio.h>
+
+int main() {
+	printf("floor of 2.3 is %.1lf\n", floor(2.3));
+	printf("floor of 3.8 is %.1lf\n", floor(3.8));
+	printf("floor of -2.3 is %.1lf\n", floor(-2.3));
+	printf("floor of -3.8 is %.1lf\n", floor(-3.8));
+	_getch();
+	return 0;
+}
